7ce15c8af2578d8210df8006b09d0a616695bd4f
weiliya <viktoria.szabo.cv@gmail.com>
2019-02-20 13:52:08 +0100

    environment set

---
+++ miniUserRESTApi/src/server/defaultState.js
@@ -0,0 +1,13 @@
+export const defaultState = {
+    user:[{
+        id: "U1",
+        name: "Dev",
+        email: "dev@dev.com"
+    }, {
+        id: "U2",
+        name: "Rob",
+        email: "rob@rob.com"
+    }
+
+    ]
+}
